20af177815c58b89a45d0b6ac6a7fe5b901e63f5
sinchanahegde-15 <sinchanachandrashekar15@gmail.com>
2026-05-10 11:34:17 +0530

    Refactor App to use headless Chrome with WebDriverManager
    Updated main method to use ChromeOptions for headless execution and added WebDriverManager setup.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,20 +3,34 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) throws InterruptedException
     {
-        WebDriver driver=new ChromeDriver();
-        driver.get("https://automationexercise.com/products");
-        driver.manage().window().maximize();
-        driver.findElement(By.id("search_product")).sendKeys("Blue top");
-        driver.findElement(By.id("submit_search")).click();
-    }
-}
+        WebDriverManager.chromedriver().setup();
 
+        ChromeOptions options = new ChromeOptions();
+
+        
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://automationexercise.com/products");
+
+        driver.findElement(By.id("search_product")).sendKeys("blue top");
+        driver.findElement(By.id("submit_search")).click();
+
+        Thread.sleep(2000);
+
+        
+        System.out.println("Page Title: " + driver.getTitle());
+
+        driver.quit();
+    }
